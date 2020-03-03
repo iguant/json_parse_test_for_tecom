3c07b337cff460ce569c563ee010e71f9600f6e8
Anton Markelov <antonmarkelov@kpmg.ru>
2020-03-03 18:03:00 +0300

    Added settings reading

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -1,5 +1,6 @@
 class Crawler:
-    def __init__(self, search_pattern: str, p_count: int):
-        pass
+    def __init__(self, search_string: str):
+        self.search_string = search_string
 
     def process(self):
+        pass
--- main.py
+++ main.py
@@ -1,3 +1,28 @@
+import argparse
+from pathlib import Path
+
+import yaml
+
+from crawler.crawler import Crawler
+
 if __name__ == '__main__':
 
+    def get_arrgs() -> argparse.Namespace:
+        parser = argparse.ArgumentParser(description='Crawler')
+        parser.add_argument('--path', type=Path, help='Path to yaml settings file', default='./settings.yaml')
+        return parser.parse_args()
+
+
+    args = get_arrgs()
+    assert args.path.is_file(), "File not found"
+    with open(args.path) as settings_file:
+        try:
+            settings = yaml.load(settings_file, Loader=yaml.BaseLoader)
+            search_string = settings["settings"]["search_string"]
+            p_count = int(settings["settings"]["pages_count"])
+        except:
+            raise "Not able to load settings"
+    assert p_count > 0 and len(search_string) != 0, "Incorrect settings"
+
+    crawler = Crawler(search_string=search_string)
     crawler.process()
