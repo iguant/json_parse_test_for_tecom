ce2f382ccbea24b5c751aadd15344c0b27921cac
Anton Markelov <antonmarkelov@kpmg.ru>
2020-03-04 17:48:39 +0300

    Added site parsing

--- crawler/crawler.py
+++ crawler/crawler.py
@@ -40,24 +40,56 @@ class Crawler:
         res = service.cse().list(q=self.search_string, num=self.p_count, cx=self.cse_id, **kwargs).execute()
         return res
 
+    def _parce_json(self, json_data):
+        result = {}
+
+        def nested_recurse_json(piece: dict):
+            for key in piece:
+                if type(piece[key]) is str:
+                    result[key] = piece[key]
+                if type(piece[key]) is dict:
+                    nested_recurse_json(piece[key])
+
+        nested_recurse_json(json_data)
+        return result
+
     def _parse_html(self, html):
-        pass
+        result_json = {}
+        found = 0
 
-
-
-
-
-
-
-
+        def nested_recurse_text(piece_text: str, found):
+            open_bracket = piece_text.find('{')
+            substring = piece_text[open_bracket:]
+            pointer = 1
+            for char in range(1, len(substring)):
+                if substring[char] is "{":
+                    pointer += 1
+                if substring[char] is "}":
+                    pointer -= 1
+                if pointer is 0:
+                    found += 1
+                    try:
+                        pre_json = json.loads(substring[:char + 1])
+                        print("Loaded JSON")
+                        parsed_json = self._parce_json(json_data=pre_json)
+                        json_object = f"object_{found}"
+                        result_json[json_object] = parsed_json
+                    except:
+                        print("Cannot load JSON")
+                    if len(substring[char:]) > 1:
+                        nested_recurse_text(substring[char:], found)
+                    break
+        nested_recurse_text(html.text, found)
+        return result_json
 
     def process(self) -> list:
         search_results = self._google_search()
         for item in search_results["items"]:
             url = item["link"]
+            print(item["link"])
             session = self._prepare_http_adapter(url)
             html = self._get_html(session=session, url=url)
-            self.result.append((item["title"], self._parse_html(html=html)))
+            content = self._parse_html(html=html)
+            if len(content) != 0:
+                self.result.append((item["title"], content))
         return self.result
-
-
